ae97228556c99657fa61279650ebadb564df21dd
micahjones13 <micahjones13@gmail.com>
2019-06-24 12:10:50 -0600

    finished basic login and navBar

--- tabthursday/src/App.js
+++ tabthursday/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import NavBar from './components/navBar';
+import Login from './components/Login';
+
 import './App.css';
 
+//Navlink route, login route
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Route path = '/login' component = { Login } />
     </div>
   );
 }
