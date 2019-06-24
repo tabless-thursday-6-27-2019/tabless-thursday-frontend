010525649040ba766e4da76b003cafd02b60c0b6
Full-lifey <44554249+Full-lifey@users.noreply.github.com>
2019-06-24 15:05:34 -0600

    build map on tabslist

--- tabthursday/src/components/TabsList.js
+++ tabthursday/src/components/TabsList.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class TabsList extends React.Component {
+  state = {
+    tabs: []
+  };
   componentDidMount() {}
   addTab = e => {
     e.preventDefault();
   };
   render() {
-    return <h1>showing tabs bitchezzzzz</h1>;
+    return (
+      <div>
+        <h1>showing tabs bitchezzzzz</h1>
+        {this.state.tabs.map(tab => {
+          <h4>{tab.name}</h4>;
+        })}
+      </div>
+    );
   }
 }
 
